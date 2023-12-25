328c78e0eb31941c333aeb68090911591988c3d8
Saikat <sachinsingha7899@gmail.com>
2023-12-25 18:19:19 +0600

    implemented delete and update

--- index.js
+++ index.js
@@ -25,18 +25,100 @@ async function run() {
     const taskCollection = client.db("TaskManagement").collection("tasks");
 
     app.post("/taskInfo", async (req, res) => {
-        try {
-          const fromData = req.body;
-          console.log(req.body);
-          const result = await taskCollection.insertOne(fromData);
-          res.send(result);
-        } catch (error) {
-          console.log(error);
-        }
-      });
+      try {
+        const fromData = req.body;
+        console.log(req.body);
+        const result = await taskCollection.insertOne(fromData);
+        res.send(result);
+      } catch (error) {
+        console.log(error);
+      }
+    });
+
+    app.get("/tasks", async (req, res) => {
+      const email = req.query?.email;
+      // || 'saikatsingha50@gmail.com'
+      const query = {
+        user_email: email,
+      };
+
+      const result = await taskCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    // app.put("/tasksUpdate/:id", async (req, res) => {
+    //   try {
+    //     const taskId = req.params.id;
+    //     console.log(taskId);
+    //     const updatedTaskData = req.body;
+    //     console.log(updatedTaskData);
+    //     const filter = { _id: new ObjectId(taskId) };
+    //     const updatedTask = {
+    //       taskName: updatedTaskData.taskName,
+    //       description : updatedTaskData.description,
+    //       deadline:updatedTaskData.deadline,
+    //       priority:updatedTaskData.priority,
+    //       status: updatedTaskData.status
+    //     };
+    //     const updateOperation = {
+    //       $set: updatedTask,
+    //     };
+    //     const result = await taskCollection.updateOne(filter, updateOperation);
+    //     res.send(result);
+
+    //     // // Find and update the task by its ID
+    //     // const updatedTask = await Task.findByIdAndUpdate(taskId, updatedTaskData, { new: true });
+
+    //     // if (!updatedTask) {
+    //     //   return res.status(404).json({ message: 'Task not found' });
+    //     // }
+
+    //     // res.json(updatedTask);
+    //   } catch (error) {
+    //     console.error("Error updating task:", error);
+    //     res.status(500).json({ message: "Internal server error" });
+    //   }
+    // });
 
 
+    app.patch('/tasksUpdate/:id', async (req, res) => {
+      const taskId = req.params.id;
+      console.log(taskId);
+      const updates = req.body;
+      console.log(updates);
+      const filter = { _id: new ObjectId(taskId) };
+      const options = { upsert: true };
+      // const updatedTaskData = {
+      //   taskName: req.body.taskName,
+      //   description: req.body.description,
+      //   deadline: req.body.deadline,
+      //   priority: req.body.priority,
+      //   taskType: req.body.status,
+      // };
+      const updateOperation = {
+        $set: updates,
+      };
+      try {
+        
+        
+    
+        // Find and update the task by its ID
+        const result = await taskCollection.updateOne(filter, updateOperation,options);
+        console.log(result);
+        res.send(result);
+      } catch (error) {
+        console.error('Error updating task:', error);
+        res.status(500).json({ message: 'Internal server error' });
+      }
+    });
 
+    app.delete("/tasksDelete/:id", async (req,res) => {
+      const id = req.params;
+      const query = { _id: new ObjectId(id) };
+      const result = await taskCollection.deleteOne(query);
+      console.log(result);
+      res.send(result);
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
